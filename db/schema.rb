81c841568d9796f427777ea8ed04bb43c34d2e35
Clothilde Drouet clothildedrouet@gmail.com <clothildedrouet@gmail.com>
2024-04-25 12:26:58 +0200

    add filename

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_24_121945) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_25_102618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_24_121945) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "cover_path"
+    t.string "cover_filename"
   end
 
   create_table "users", force: :cascade do |t|
